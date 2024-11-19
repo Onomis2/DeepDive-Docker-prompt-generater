ffb7ee78b83c456970af809eb8e9a2f4653d0b39
Joshua Kool <jmdkool@hotmail.com>
2024-11-19 10:19:01 +0100

    file

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
 header("Location:src/homepage.php");
+die;
 ?>
--- src/homepage.php
+++ src/homepage.php
@@ -13,11 +13,14 @@ $subjects = array('code', 'bakken', 'storytelling', 'b', 'p', 'j', 'h', 'y', 't'
 </head>
 <body>
     <div class="title-center-home">
-        <h1>Home</h1>
+        <h1>Choose a subject</h1>
+        <nav class="right-button-login">
+            <a href="login.php"><button class="login-button">Login</button></a>
+        </nav>
     </div>
     <div class="center-boxes">
         <?php foreach ($subjects as $subject) { ?>
-            <p class="boxes"><?= $subject;?></p>
+            <a href="choise.php"><p class="boxes"><?= $subject;?></p></a>
         <?php } ?>
     </div>
 </body>
