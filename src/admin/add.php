122ac90a29041ae31872f444adbc73e38340b7d9
Joshua Kool <jmdkool@hotmail.com>
2024-11-19 13:24:29 +0100

    add.php, choice.php

--- src/admin/add.php
+++ src/admin/add.php
@@ -8,6 +8,13 @@ if (empty($_SESSION["loggedInUser"])) {
     header("Location: ../login-logout/login.php");
     die;
 }
+
+$Database = $dbh->prepare("SELECT * FROM subjects ORDER BY id");
+$Database->execute();
+$subjects = $Database->fetchAll(PDO::FETCH_ASSOC);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +25,17 @@ if (empty($_SESSION["loggedInUser"])) {
     <title>add prompts</title>
 </head>
 <body>
-    
+    <nav class="right-button-login">
+        <a href="../login-logout/logout.php"><button class="login-button">Logout</button></a>
+        <a href="../homepage.php"><button class="back">Back</button></a>
+    </nav>
+    <div class="title-center-home">
+        <h1>Add a prompt</h1>
+    </div>
+    <form method="post">
+        <?php foreach ($subjects as $subject) { ?>
+            <input type="radio" name="subject" value="<?= $subject['id']; ?>"><?= $subject['subject']; ?>
+        <?php } ?>
+    </form>
 </body>
 </html>
